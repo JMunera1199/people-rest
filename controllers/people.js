f6626bae27d531b1c806b1b8ad8b55dc27bb4a6b
JMunera1199 <38870939+JMunera1199@users.noreply.github.com>
2021-08-08 09:59:25 -0400

    Made updates to endpoint names and functionality

--- controllers/people.js
+++ controllers/people.js
@@ -1,31 +1,31 @@
-const { Console } = require('console');
-const { Validator } = require('jsonschema');
 var people = require('../model/peopleModel.js');
 var persons = require(people.getFileLocation(true));
 const fs = require('fs');
 var ssns = persons.map(getSSN);
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = (req, res) => {
   res.status(200).json({
     posts: persons
   });
 
 };
 
-exports.getPostsBySSN = (req, res, next) => {
-  persons.forEach(element => {
+exports.getPostsBySSN = (req, res) => {
+  if (persons.length > 0) {
+    console.log(persons);
+    persons.forEach(element => {
 
-    if (req.params.socialSecurityNumber == element.socialSecurityNumber) {
-      res.status(200).json({
-        posts: persons
-      });
-    } else {
-      res.status(404).json({
-        message: '404 Not Found Error'
-      })
-    }
-  })
-
+      if (req.params.socialSecurityNumber == element.socialSecurityNumber) {
+        res.status(200).json({
+          posts: element
+        });
+      }
+    })
+  } else {
+    res.status(404).json({
+      message: '404 Not Found Error'
+    })
+  }
 };
 
 exports.createPost = (req, res) => {
@@ -73,23 +73,17 @@ exports.updatePut = (req, res) => {
   })
 };
 
-exports.deletePostsBySSN = (req, res, next) => {
-
-  const firstName = req.body[0].firstName;
-  const lastName = req.body[0].lastName;
-  const dateOfBirth = req.body[0].dateOfBirth;
-  const emailAddress = req.body[0].emailAddress;
-  const socialSecurityNumber = req.body[0].socialSecurityNumber;
+exports.deletePostsBySSN = (req, res) => {
+  
   const socialSecurityNumberParam = req.params.socialSecurityNumber;
 
-  if ((ssns.includes(socialSecurityNumberParam)) && (socialSecurityNumber == socialSecurityNumberParam)) {
-    persons = persons.filter(({ socialSecurityNumber }) => socialSecurityNumber !== req.params.socialSecurityNumber);
+  if ((ssns.includes(socialSecurityNumberParam))) {
+    persons = persons.filter(({ socialSecurityNumber }) => socialSecurityNumber != socialSecurityNumberParam);
 
     fs.writeFileSync(people.getFileLocation(false), JSON.stringify(persons));
 
     res.status(200).json({
-      message: 'Delete successful!',
-      post: { firstName: firstName, lastName: lastName, dateOfBirth: dateOfBirth, emailAddress: emailAddress, socialSecurityNumber: "xxx-xx-" + socialSecurityNumber.substr(7, 9) }
+      message: 'Delete successful!'
     });
   } else {
     res.status(400).json({
--- routes/people.js
+++ routes/people.js
@@ -5,16 +5,16 @@ const peopleController = require('../controllers/people');
 const router = express.Router();
 
 // GET /people/posts
-router.get('/posts', peopleController.getPosts);
-router.get('/posts/:socialSecurityNumber', peopleController.getPostsBySSN);
+router.get('/person', peopleController.getPosts);
+router.get('/person/:socialSecurityNumber', peopleController.getPostsBySSN);
 
 // POST /people/post
-router.post('/post', peopleController.createPost);
+router.post('/person', peopleController.createPost);
 
 // PUT /people/put
-router.put('/put/:socialSecurityNumber', peopleController.updatePut);
+router.put('/person/:socialSecurityNumber', peopleController.updatePut);
 
 // DELETE /people/delete
-router.delete('/delete/:socialSecurityNumber', peopleController.deletePostsBySSN);
+router.delete('/person/:socialSecurityNumber', peopleController.deletePostsBySSN);
 
 module.exports = router;
